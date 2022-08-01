ff13b412549372f951d3de00a3e9e20f48b825d4
João Pedro De Arruda Rocha <Joo.Rocha@proway.treina>
2022-08-01 16:33:11 -0300

    splash

--- lib/shared/widgets/splash_screen.dart
+++ lib/shared/widgets/splash_screen.dart
@@ -11,9 +11,13 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  bool isStarted = false;
+
   @override
   void initState() {
+    isStarted = false;
     super.initState();
+
     Timer(
       const Duration(seconds: 3),
       () {
@@ -25,6 +29,15 @@ class _SplashPageState extends State<SplashPage> {
         );
       },
     );
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      changeStarted();
+    });
+  }
+
+  void changeStarted() async {
+    setState(() {
+      isStarted = !isStarted;
+    });
   }
 
   @override
@@ -32,25 +45,37 @@ class _SplashPageState extends State<SplashPage> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 22, 22, 22),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'HabitsNow',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 214, 32, 90),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text(
+                    'HabitsNow',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 214, 32, 90),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(
+                      Color.fromARGB(255, 214, 32, 90),
+                    ),
+                    strokeWidth: 11.0,
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation(
-                Color.fromARGB(255, 214, 32, 90),
-              ),
-              strokeWidth: 11.0,
+            AnimatedContainer(
+              color: isStarted
+                  ? Colors.transparent
+                  : const Color.fromARGB(255, 22, 22, 22),
+              duration: const Duration(milliseconds: 750),
             ),
           ],
         ),
